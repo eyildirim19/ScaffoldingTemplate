cfd36b5edcd734dc3d43d38df994ae9a8e746c88
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2022-09-24 13:35:05 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScaffoldingTemplate.Models;
+
+namespace ScaffoldingTemplate.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<User> userList = new List<User>();
+        public HomeController()
+        {
+            userList.Add(new User { UserId = "1", UserName = "eyildirim", Password = "123", Name = "Ekrem", SurName = "Yıldırım" });
+            userList.Add(new User { UserId = "2", UserName = "bgenc", Password = "321", Name = "Büşra", SurName = "Genç" });
+            userList.Add(new User { UserId = "3", UserName = "oderman", Password = "213", Name = "Onur", SurName = "Derman" });
+        }
+
+        //Action name sağ tık yapıp addview diyebilirsiniz. 
+        public IActionResult Index()
+        {
+            return View(userList);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(User model)
+        {
+            return View();
+        }
+
+        public IActionResult Edit(string id)
+        {
+            var model = userList.FirstOrDefault(c => c.UserId == id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(User model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ScaffoldingTemplate.Models
+{
+    public class User
+    {
+        public string UserId { get; set; }
+
+        [Display(Name = "Kullanıcı Adı")]
+        [Required(ErrorMessage = "Kullanıcı Aıd alanı zorunludur")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Kullanıcı Adı alanı min. 3 karakter max. 20 karakter olabilir")]
+        public string UserName { get; set; }
+
+
+        [Display(Name = "Şifre")]
+        [Required(ErrorMessage = "Şifre alanı zorunludur")]
+        [StringLength(10, MinimumLength = 3, ErrorMessage = "Şifre Adı alanı min. 3 karakter max. 10 karakter olabilir")]
+        public string Password { get; set; }
+
+        [Display(Name = "Adı")]
+        public string Name { get; set; }
+
+        [Display(Name = "SoyAdı")]
+        public string SurName { get; set; }
+    }
+}
